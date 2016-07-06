81bc97625ec1aad7f909264814e8b9b5f50eb740
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2016-07-06 22:11:56 +0200

    refactor(dspBot): refactoriza el código y añade nuevas funciones

--- app.js
+++ app.js
@@ -12,12 +12,30 @@ app.listen(3000, function () {
 var Discord = require("discord.js");
 
 var mybot = new Discord.Client();
+var command = {
+  test: test,
+  imagen: imagen
+};
 
 mybot.on("message", function(message) {
-    if(message.content === "ping") {
-        mybot.reply(message, "pong");
+  if (message.content.indexOf("&") === 0 && message.content.length > 1) {
+    if (message.content.split("-").length > 1) {
+      command[message.content.split("&")[1].split("-")[0]](message);
+    } else {
+      command[message.content.split("&")[1]](message);
     }
+  }
 });
+
+function test(message) {
+  mybot.sendMessage(message, "TEST");
+}
+
+function imagen(message) {
+  if (message.content.split("-").length > 1) {
+    mybot.sendMessage(message, "https://www.google.es/search?q=" + message.content.split("-")[1] + "&espv=2&biw=1920&bih=955&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiXnZnfy9_NAhVCPRQKHcK7BUcQ_AUIBigB");
+  }
+}
 
 mybot.loginWithToken("MjAwMzE0ODg4NDAzNDE5MTM4.Cl7mmA.-5feNXiIC6WbUt4b-uAVtt0rDt0");
 // If you still need to login with email and password, use mybot.login("email", "password");
