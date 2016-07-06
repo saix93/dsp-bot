ffe3d8912dd0461076803237b4066d88b307f594
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2016-07-06 21:01:45 +0200

    feat(dspBot): añade la funcionalidad básica de ping-pong

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+var Discord = require("discord.js");
+
+var mybot = new Discord.Client();
+
+mybot.on("message", function(message) {
+    if(message.content === "ping") {
+        mybot.reply(message, "pong");
+    }
+});
+
+mybot.loginWithToken("MjAwMzE0ODg4NDAzNDE5MTM4.Cl7mmA.-5feNXiIC6WbUt4b-uAVtt0rDt0");
+// If you still need to login with email and password, use mybot.login("email", "password");
