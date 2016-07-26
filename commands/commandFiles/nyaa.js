ff2a79dad5489256a83e622156223830f0febf55
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2016-07-26 14:17:48 +0200

    feat(nyaa): permite recoger las opciones del comando juntas (-f, -hs, -fhs...)

--- commands/commandFiles/nyaa.js
+++ commands/commandFiles/nyaa.js
@@ -17,41 +17,63 @@ Usage:
 
 Options:
     -h --help   _Shows this screen_
-    -f --first  _Returns the first download link found_`;
+    -f --first  _Returns the first download link found_
+    -hs --horsubs  _Returns the first download link found of horrible subs in 1080p_
+    -fhs --firsthorsubs  _Returns the first download link found of horrible subs in 1080p_`;
 
 var commandProperties = {
   name: commandName,
   description: 'Search for an anime in nyaa',
   help: help,
   level: 0,
-  fn: doGreet
+  fn: doNyaa
 };
 
 var command = new Command(commandProperties);
 
-function doGreet(message, client, args) {
-  var anime = "";
-  var link;
-
-  args.params.forEach(function(val, index) {
-    anime = anime + val.split(" ").join("+") + "+";
-  });
-
-  anime = anime.substring(0, anime.length - 1);
-
-  if (args.flags['horsubs'] || args.options['hs']) {
-    link = `https://www.nyaa.se/?page=search&cats=0_0&filter=0&term=${anime}+horrible+subs`;
-  } else {
-    link = `https://www.nyaa.se/?page=search&cats=0_0&filter=0&term=${anime}`;
+// Coge todas las options que se han pasado al comando y comprueba qué opciones hay entre ellas
+function takeOptions(obj) {
+  var result = {};
+  for (var p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      if (p.indexOf("f") !== -1 || p.indexOf("first") !== -1) {
+        result["first"] = true;
+      }
+      if (p.indexOf("hs") !== -1 || p.indexOf("horsubs") !== -1) {
+        result["horsubs"] = true;
+      }
+    }
   }
+  return result;
+}
 
-  if (args.flags['first'] || args.options['f']) {
-    request(link, function(error, response, html) {
-      var $ = cheerio.load(html);
-      client.reply(message, "https:" + $(".tlistdownload a").attr("href"));
+function doNyaa(message, client, args) {
+  if (args.params.length > 0) {
+    var anime = "";
+    var horsubs = "";
+    var link;
+    var options = takeOptions(args.options);
+
+    args.params.forEach(function(val, index) {
+      anime = anime + val.split(" ").join("+") + "+";
     });
-  } else {
-    client.reply(message, link);
+
+    anime = anime.substring(0, anime.length - 1);
+
+    if (options["horsubs"]) {
+      horsubs = "+horrible+subs+1080";
+    }
+
+    link = `https://www.nyaa.se/?page=search&cats=0_0&filter=0&term=${anime}${horsubs}`;
+
+    if (options["first"]) {
+      request(link, function(error, response, html) {
+        var $ = cheerio.load(html);
+        client.reply(message, "https:" + $(".tlistdownload a").attr("href"));
+      });
+    } else {
+      client.reply(message, link);
+    }
   }
 }
 
