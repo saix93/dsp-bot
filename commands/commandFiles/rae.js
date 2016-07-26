499dd5bb22ec3497e9825e04dd8a5bed38a0e653
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2016-07-26 18:38:57 +0200

    refactor(rae): elimina código basura y comenta el comando

--- commands/commandFiles/rae.js
+++ commands/commandFiles/rae.js
@@ -33,32 +33,31 @@ function doRae(message, client, args) {
   if (args.params.length > 0) {
     var url = `http://dle.rae.es/srv/search?w=${args.params[0]}`;
 
-    if (args.flags['first'] || args.options['f']) {
-      //
-    } else {
-      request(url, function(error, response, html) {
-        url = url.replace("srv/search", "");
-        var $ = cheerio.load(html);
-        if ($("ul").length > 0) {
-          client.reply(message, `Lo siento pero vas a tener que utilizar el enlace: ${url}`);
-        } else {
-          if ($(".f")[0] && $(".f")[0].children[0]) {
-            var word = $(".f")[0].children[0].data;
-            html = html.replace(word, `**${word}**`);
-          }
-          var text = $(`<div>${toMarkdown(html)}</div>`).text();
+    // Llamada a la url del parámetro indicado
+    request(url, function(error, response, html) {
+      url = url.replace("srv/search", "");
+      var $ = cheerio.load(html);
 
-          // Se responde al usuario
-          if (text.length > 500) {
-            text = text.substr(0, 500) + "...\n\n**Más: **" + url;
-            client.reply(message, text);
-          } else {
-            client.reply(message, text);
-          }
+      // Comprueba si hay más de una palabra como resultado
+      if ($("ul").length > 0) {
+        client.reply(message, `Lo siento pero vas a tener que utilizar el enlace: ${url}`);
+      } else {
+        // Añade negrita a la palabra
+        if ($(".f")[0] && $(".f")[0].children[0]) {
+          var word = $(".f")[0].children[0].data;
+          html = html.replace(word, `**${word}**`);
         }
-      });
-      //client.reply(message, url);
-    }
+        var text = $(`<div>${toMarkdown(html)}</div>`).text();
+
+        // Si el texto tiene más de 500 caracteres, se corta y se introduce el link al final
+        if (text.length > 500) {
+          text = text.substr(0, 500) + "...\n\n**Más: **" + url;
+          client.reply(message, text);
+        } else {
+          client.reply(message, text);
+        }
+      }
+    });
   }
 }
 
