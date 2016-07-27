7289beee5dc7932f8c0e5bb147c16eea664fc5f2
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2016-07-27 13:12:02 +0200

    feat(say): desarrolla comando que hace que el bot lea lo que se le escribe, falta probarlo (no puedo probar el audio del bot)

---
+++ commands/commandFiles/say.js
@@ -0,0 +1,52 @@
+'use strict';
+var Command = require('../../lib/command.js'),
+    config = require('../../lib/ConfigManager.js').config;
+
+var request = require('request');
+
+var prefix = config.settings.prefix,
+    commandName = 'say';
+
+var help = `**${commandName.toUpperCase()}**
+_Do the DSP-BOT say a phrase or word_
+
+Usage:
+    ${prefix}${commandName} <word or phrase>
+    ${prefix}${commandName} -h | --help
+
+Options:
+    -h --help   _Shows this screen_`;
+
+var commandProperties = {
+  name: commandName,
+  description: 'Say a word or phrase',
+  help: help,
+  level: 0,
+  fn: doSay
+};
+
+var command = new Command(commandProperties);
+
+function doSay(message, client, args) {
+  if (args.params.length > 0) {
+    var messageToTranslate;
+    var url = "https://translate.google.com/translate_tts?ie=UTF-8&q=REPLACEME&tl=es-ES&client=tw-ob";
+
+    args.params.forEach(function(val, index) {
+      messageToTranslate = messageToTranslate + val.split(" ").join("+") + "+";
+    });
+
+    messageToTranslate = messageToTranslate.substring(0, messageToTranslate.length - 1);
+
+    url = url.replace("REPLACEME", messageToTranslate);
+
+    request(url, function(error, response, html) {
+      if (client.internal.voiceConnection) {
+        client.voiceConnection.setSpeaking(true);
+        client.voiceConnection.playRawStream(html, { volume: 0.25 });
+      }
+    })
+  }
+}
+
+module.exports = command;
