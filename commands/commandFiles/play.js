a6ce9ede1cf8dd926a18ac5e10de833e05f2144d
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2018-04-18 16:46:09 +0200

    Añade funcionalidad de mostrar una lista de ficheros y control de errores al comando play

--- commands/commandFiles/play.js
+++ commands/commandFiles/play.js
@@ -1,6 +1,7 @@
 'use strict';
 var Command = require('../../lib/command.js'),
-    config = require('../../lib/ConfigManager.js').config;
+    config = require('../../lib/ConfigManager.js').config,
+    fs = require('fs');
 
 var prefix = config.settings.prefix,
     commandName = 'play';
@@ -9,11 +10,13 @@ var help = `**${commandName.toUpperCase()}**
 _Do the DSP-BOT play an audio file_
 
 Usage:
-    ${prefix}${commandName} <word or phrase>
+    ${prefix}${commandName} <word or phrase (name of the file)>
     ${prefix}${commandName} -h | --help
+    ${prefix}${commandName} -l | --list
 
 Options:
-    -h --help   _Shows this screen_`;
+    -h --help   _Shows this screen_
+    -l --list   _Lists all available files_`;
 
 var commandProperties = {
   name: commandName,
@@ -25,12 +28,34 @@ var commandProperties = {
 
 var command = new Command(commandProperties);
 
-function doPlay(message, client, args) {
+function doPlay(message, client, args, options) {
+  if (options['l'] || options['list']) {
+    var msg = "**List of stored mp3 files:**\n";
+
+    fs.readdir('.\\Audio', function (err, files) {
+      for (var i =  0; i < files.length; i++) {
+        msg += `*- ${files[i].slice(0, -4)}*\n`;
+      }
+
+      message.channel.send(msg);
+    });
+
+    return;
+  }
+
   if (args.params.length > 0) {
     const connection = client.voiceConnections.first();
     
     if (connection) {
-      connection.playFile(`.\\Audio\\${args.params.join(" ")}.mp3`);
+      var path = `.\\Audio\\${args.params.join(" ")}.mp3`;
+      if (fs.existsSync(path)) {
+        connection.playFile(path);
+        message.channel.send('Playing!');
+      } else {
+        throw new Error('The file doesn\' exist');
+      }
+    } else {
+      throw new Error(`I am not connected to any voice channel! Use ${prefix}joinme first!`);
     }
   }
 }
