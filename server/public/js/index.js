b8852a634cfdbebb8f5523573340dff0ea5a7216
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2018-04-23 19:56:36 +0200

    Recupera la configuración del bot

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,18 +1,44 @@
+let config = {};
+
 $.ajax(
   {
-    url: "/commandList",
-    success: function(result) {
-      $("#commandList").append(function() {
-        let listString = "<ul>";
-
-        for (var propName in result) {
-          listString += `<li>${propName}</li>`;
-        }
-        
-        listString += "</ul>"
-
-        return listString;
-      });
-    }
+    url: "/config",
+    success: getConfig
   }
 );
+
+function getConfig(result) {
+  config = result;
+
+  $.ajax(
+    {
+      url: "/commandList",
+      success: function(result) {
+        $("#commandList").append(function() {
+          let listString = "<h3>Command list:</h3><ul>";
+  
+          for (var propName in result) {
+            listString += `<li><a href="#${propName}">${propName}</a></li>`;
+          }
+  
+          listString += "</ul>"
+  
+          return listString;
+        });
+  
+        for (var propName in result) {
+          let properties = result[propName].properties;
+          $("#commandList").append(createCommandBlock(properties.name, properties.description));
+        }
+      }
+    }
+  );
+}
+
+function createCommandBlock(name, description) {
+  let command = "<hr></hr>";
+  command += `<h3 id="${name}">${config.settings.prefix}${name}</h3>`;
+  command += `${description}`;
+
+  return command;
+}
