fda681ed637637416028f37814afdf9fde9ba60c
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2018-04-27 23:56:11 +0200

    Revierte los cambios hechos para el testing de RedHat Openshift

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var http	      = require('http');
 var express 		= require('express');
 var app 		    = express();
 
-var webServer = http.createServer(app).listen(8080);
+var webServer = http.createServer(app).listen(80);
 
 function start() {
   app.use('/', express.static('server/public'));
