19f8ef8050670672e43a7cf633c6923c1f517c30
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2018-04-27 23:44:58 +0200

    cambia el puerto del 80 al 8080

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var http	      = require('http');
 var express 		= require('express');
 var app 		    = express();
 
-var webServer = http.createServer(app).listen(80);
+var webServer = http.createServer(app).listen(8080);
 
 function start() {
   app.use('/', express.static('server/public'));
